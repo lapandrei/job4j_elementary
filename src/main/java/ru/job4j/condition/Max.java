1a4ab7da24277887967b14fd272d147d74b7fb95
Андрей <lapihin.andrei@yandex.ru>
2021-10-14 09:25:01 +0300

    4.2. Тернарное сравнение [#189 #215545]

--- src/main/java/ru/job4j/condition/Max.java
+++ src/main/java/ru/job4j/condition/Max.java
@@ -2,13 +2,11 @@ package ru.job4j.condition;
 
 public class Max {
     public static int max(int left, int right) {
-        boolean condition = left >= right;
-        int result = condition ? left : right;
-        return result;
+        return left > right ? left : right;
     }
 
     public static void main(String[] args) {
-        int left = 35;
+        int left = 20;
         int right = 20;
         System.out.println("Из чисел " + left + " и " + right + ", больше " + Max.max(left, right));
     }
--- src/test/java/ru/job4j/condition/MaxTest.java
+++ src/test/java/ru/job4j/condition/MaxTest.java
@@ -8,7 +8,7 @@ public class MaxTest {
     @Test
     public void when1To2Then2() {
         int left = 1;
-        int right = 2;
+        int right = 3;
         int result = Max.max(left, right);
         int expected = 2;
         Assert.assertEquals(result, expected);
