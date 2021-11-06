2dfa2b864a0f9eab3e3b6d82640f7a7cdc66ea66
Андрей <lapihin.andrei@yandex.ru>
2021-11-06 12:26:38 +0300

    1. Чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,6 @@ public class ArrayChar {
         boolean result = true;
         for (int index = 0; index < pref.length; index++) {
             if (word[index] != pref[index]) {
-
                 return false;
             }
         }
--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -5,23 +5,18 @@ public class ArrayDefinition {
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
         System.out.println("Размер ages равен " + ages.length);
         System.out.println("Размер surnames равен " + surnames.length);
         System.out.println("Размер prices равен " + prices.length);
         System.out.println();
-
         String[] names = new String[4];
-
         names[0] = "Petr Arsentev";
         names[1] = "Andrei Lapihin";
         names[2] = "Mikki Maus";
         names[3] = "Vladimir Lenin";
-
         System.out.println(names[0]);
         System.out.println(names[1]);
         System.out.println(names[2]);
         System.out.println(names[3]);
-
     }
 }
