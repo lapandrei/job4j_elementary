89bdb3da0521b928627df6c79d71fce9efb7a280
lapandrei <lapihin.andrei@yandex.ru>
2022-12-21 20:25:59 +0300

    6.9. String - это массив [#504884]

---
+++ src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -0,0 +1,34 @@
+package ru.job4j.array;
+
+public class JavaNameValidator {
+    public static boolean isNameValid(String name) {
+        boolean rsl = false;
+        if (!name.isEmpty() && isLowerLatinLetter(name.codePointAt(0))) {
+            for (int i = 1; i < name.length(); i++) {
+                int code = name.codePointAt(i);
+                if (isSpecialSymbol(code) || isLowerLatinLetter(code)
+                        || isUpperLatinLetter(code) || Character.isDigit(code)) {
+                    rsl = true;
+
+                }
+                else {
+                    rsl = false;
+                    break;
+                }
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean isSpecialSymbol(int code) {
+        return code == 36 || code == 95;
+    }
+
+    public static boolean isUpperLatinLetter(int code) {
+        return code >= 65 && code <= 90;
+    }
+
+    public static boolean isLowerLatinLetter(int code) {
+        return code >= 97 && code <= 122;
+    }
+}
